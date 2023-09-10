1c77a243930ea1366823407a029f231a6ec965ad
RickiBobbii <rickibobbiii@gmail.com>
2023-09-10 19:12:35 -0400

    fix flow of javascript and textarea issue

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,19 @@
-// Assignment Code
-/*var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-*/
-
-var lowercase = confirm('Would you like to include lowercase characters?');
-var uppercase = confirm('Would you like to include uppercase characters?');
-var numeric = confirm('Would you like to include numeric characters?');
-var special = confirm('Would you like to include special characters?');
+//5 prompt variables
+var lowercase = confirm('Would you like to include Lowercase characters?');
+var uppercase = confirm('Would you like to include Uppercase characters?');
+var numeric = confirm('Would you like to include Numeric characters?');
+var special = confirm('Would you like to include Special characters?');
 // parseInt converts string input into an Integer
 var length = parseInt(prompt('How long do you want your password to be?'));
+//variables to hold all inputs and the generated password
+var all = '';
+var password = '';
+//variables to select the proper id from html to generate and display password
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 if (lowercase || uppercase || numeric || special) {
-  // At least one option is selected
-  var all = '';
+  // At least one option is selected, adds all strings together that are selected
   if (lowercase) {
     all += 'abcdefghijklmnopqrstuvwxyz';
     console.log(all);
@@ -41,38 +30,25 @@ if (lowercase || uppercase || numeric || special) {
     all += '!@#$%^&*()_+{}:\"<>?\\|[];\',./`~';
     console.log(all);
   }
-  var password = '';
+  } else {
+    // No option is selected
+    console.log('User did not select any option.');
+  }
+
+//event listerner to generate password on click on button
+generateBtn.addEventListener("click", function(event) {
+  //clear previous password
+  password = "";
   for (var i = 0; i < length; i++) {
     // charAt returns the character at the specified index of the string 
     password += all.charAt(Math.floor(Math.random() * all.length));
   }
   console.log('Your password is:', password);
-} else {
-  // No option is selected
-  console.log('User did not select any option.');
-}
-
-var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-
-
-generateBtn.addEventListener("click", function(event) {
-  passwordText.value = password;
-  
-  //no option selected
+  //if no option selected
   if (!password) {
     alert("No options selected. Please refresh page to try again.");
-    
-    //Trying to replace textarea placeholder text from displaying undefined, needs fixed
-    passwordText.removeAttribute("readonly");
     passwordText.setAttribute("placeholder", "Refresh page to try again.");
-
-    console.log(passwordText);
-    //same as previous comment, this gives a way to backspace and see refresh message, needs fixed
-    passwordText.addEventListener("input", function() {
-      if (!password) {
-        passwordText.value = "";
-      }
-    } );
+    console.log(password);
   };  
+  passwordText.value = password;
 });
